10ae1d8b2f137eb55e29b5c27a0f5586195a3f93
heiang <598481337@qq.com>
2018-07-04 23:35:36 +0800

    学习koa

---
+++ app.js
@@ -0,0 +1,46 @@
+const Koa = require('koa')
+const app = new Koa()
+const views = require('koa-views')
+const json = require('koa-json')
+const onerror = require('koa-onerror')
+const bodyparser = require('koa-bodyparser')
+const logger = require('koa-logger');
+const static=require('koa-static');
+
+// const index = require('./routes/index');
+// const users = require('./routes/users');
+
+const route = require('./routes/route');
+
+// error handler
+onerror(app)
+
+// middlewares
+app.use(bodyparser({
+  enableTypes:['json', 'form', 'text']
+}))
+app.use(json())
+app.use(logger())
+app.use(static(__dirname + '/public'))
+
+app.use(views(__dirname + '/views', {
+  extension: 'ejs'
+}))
+
+// logger
+app.use(async (ctx, next) => {
+  const start = new Date()
+  await next()
+  const ms = new Date() - start
+  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
+})
+
+// routes 注入路由中间件
+app.use(route(app))
+
+// error-handling
+app.on('error', (err, ctx) => {
+  console.error('server error', err, ctx)
+});
+
+module.exports = app
---
+++ routes/route.js
@@ -0,0 +1,26 @@
+/*路由中间件*/
+const fs = require('fs');
+let router=require('koa-router');
+let filter="route.js";//需要过滤的文件
+const rootRouter="routes"; //负责将router合并起来
+function addRouters(app,router,router_dir) {
+	let importPath=router_dir==rootRouter?__dirname:(__dirname + `/${router_dir}`);
+    let  files = fs.readdirSync(importPath);
+    let js_files = files.filter((f) => {
+        return f.endsWith('.js')&&f!=filter; 
+    });
+    let filesStr="";
+    for (let f of js_files) {
+        filesStr+=`${f} `;
+        let routerFile = require(`${importPath}/${f}`);
+        app.use(routerFile.routes(), routerFile.allowedMethods())
+    }
+    console.log(`扫描路由目录:%s \n找到文件 %s`,`${router_dir}`,`${filesStr}`);
+}
+module.exports = function (app,dir) {
+    let router_dir = dir || rootRouter;
+    addRouters(app,router,router_dir);
+    return function(){
+    	console.log("调用路由中间件");
+    };
+};
